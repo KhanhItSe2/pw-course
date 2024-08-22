0c4cae5e046787d733fe79c87e14c0e6ef083aa7
K9 - Khanh Nguyen <khanhnguyenit@gmail.com>
2024-08-22 17:52:32 +0700

    Lesson 1 - 1

---
+++ tests/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  //a. Truy cập trang web: https://material.playwrightvn.com/ 
+  await page.goto('https://material.playwrightvn.com/');
+
+  //b. Kiểm tra rằng Title contains text "Tài liệu học automation test"
+  await expect(page).toHaveTitle(/Tài liệu học automation test/);
+});
+
+test('get started link', async ({ page }) => {
+  //a. Truy cập trang web: https://material.playwrightvn.com/ 
+  await page.goto('https://material.playwrightvn.com/');
+
+  //b. Click vào link chứa text: "Bài học 1: Register Page"
+  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
+
+  //c. Kiểm tra trang web có chứa thẻ heading với nội dung: "User Registration"
+  await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+
+});
