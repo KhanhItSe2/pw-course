3d430075c8201c14ae2315fe62d66929aa31aa00
K9 - Khanh Nguyen <khanhnguyenitse@gmail.com>
2024-09-24 22:22:39 +0700

    chore: modify following to feedback

--- tests/lesson-5/test1.spec.ts
+++ tests/lesson-5/test1.spec.ts
@@ -1,13 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import path from 'path';
-console.log(__dirname);
+
 test("Exercise 1", async ({ page }) => {
     await test.step("Truy cập trang https://material.playwrightvn.com/", async () => {
         await page.goto("https://material.playwrightvn.com/");
-    })
+    });
+
     await test.step("Click vào 'Bài học 1: Register Page'", async () => {
         await page.locator("//a[@href='01-xpath-register-page.html']").click();
-    })
+    });
+
     await test.step("Nhập đầy đủ các thông tin", async () => {
         await page.locator("//input[@id ='username']").fill("Khanh Nguyen");
         await page.locator("//input[@id ='email']").fill("khanhnguyenitse@gmail.com");
@@ -15,7 +17,7 @@ test("Exercise 1", async ({ page }) => {
         await page.locator("//input[@id ='reading']").check();
         await page.locator("//input[@id ='traveling']").check();
         await page.locator("//input[@id ='cooking']").check();
-        await page.locator("//select[@id ='interests']").selectOption(['technology', 'sports']);
+        await page.locator("//select[@id ='interests']").selectOption(['technology', 'sports']); //selectOption chọn Value
         await page.locator("//select[@id ='country']").selectOption('usa');
         await page.locator("//input[@id ='dob']").fill("2001-04-21");
         await page.locator("//input[@id ='profile']").setInputFiles(path.join(__dirname, 'test.txt'));
@@ -23,6 +25,9 @@ test("Exercise 1", async ({ page }) => {
         await page.locator("//input[@id ='newsletter']").check();
         await page.locator("//div[@class ='tooltip']").hover();
         await page.locator("//label[@class='switch']").click();
+    });
+
+    await test.step("Nhấn đăng ký", async () => {
         await page.locator("//button[@type='submit']").click();
-    })
+    });
 });
