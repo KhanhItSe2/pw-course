fed9ed2188cc6ed7eb257ed5f0914532ac8542cb
K9 - Khanh Nguyen <khanhnguyenit@gmail.com>
2024-09-25 21:39:02 +0700

    chore: correct full answers lesson-5

--- tests/lesson-5/test4.spec.ts
+++ tests/lesson-5/test4.spec.ts
@@ -41,7 +41,7 @@ const newsPapers = [
         title: "Nhà phát minh thuốc kháng sinh bị lãng quên 10",
         content: "Nhà phát minh thuốc kháng sinh bị lãng quên"
     },
-]
+];
 
 test("Exercise 4", async ({ page }) => {
     await test.step("Truy cập trang https://material.playwrightvn.com/", async () => {
@@ -64,17 +64,17 @@ test("Exercise 4", async ({ page }) => {
         await page.locator("//input[@id='search']").fill(newsPapers[2].title);
     });
 });
-let content;
-let title;
+
 test("Exercise 4 - Type 2", async ({ page }) => {
+    let contents, titles;
 
     await test.step("1. Access to https://vnexpress.net/khoa-hoc", async () => {
         await page.goto("https://vnexpress.net/khoa-hoc", { waitUntil: "domcontentloaded" });
     });
 
     await test.step("2. Get Titles and Contents", async () => {
-        title = await page.locator("//article/h3[@class='title-news']/a").allTextContents();
-        content = await page.locator("//article/p[@class='description']").allTextContents();
+        titles = await page.locator("//article/h3[@class='title-news']/a").allTextContents();
+        contents = await page.locator("//article/p[@class='description']").allTextContents();
     });
 
     await test.step("3. Access https://material.playwrightvn.com/ and go to Personal Notes page", async () => {
@@ -84,13 +84,13 @@ test("Exercise 4 - Type 2", async ({ page }) => {
 
     await test.step("4. Add 10 notes", async () => {
         for (let i = 0; i < 10; i++) {
-            await page.locator("//input[@id='note-title']").fill(title[i]);
-            await page.locator("//textarea[@id='note-content']").fill(content[i]);
+            await page.locator("//input[@id='note-title']").fill(titles[i]);
+            await page.locator("//textarea[@id='note-content']").fill(contents[i]);
             await page.locator("//button[@id='add-note']").click();
         }
     });
 
     await test.step("5. Search random page title page", async () => {
-        await page.locator("//input[@id='search']").fill(title[1].trim());
+        await page.locator("//input[@id='search']").fill(titles[1].trim());
     });
 });
