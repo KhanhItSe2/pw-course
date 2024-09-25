6a6e7ac903e17e26052d34c3799d02d92f235016
K9 - Khanh Nguyen <khanhnguyenit@gmail.com>
2024-09-25 21:35:05 +0700

    feat: add Exercise 4 - type 2

--- tests/lesson-5/test4.spec.ts
+++ tests/lesson-5/test4.spec.ts
@@ -47,7 +47,7 @@ test("Exercise 4", async ({ page }) => {
     await test.step("Truy cập trang https://material.playwrightvn.com/", async () => {
         await page.goto("https://material.playwrightvn.com/");
     });
-    
+
     await test.step("Click vào 'Bài học 4: Personal notes'", async () => {
         await page.locator("//a[@href='04-xpath-personal-notes.html']").click();
     });
@@ -62,5 +62,35 @@ test("Exercise 4", async ({ page }) => {
 
     await test.step("b.Thực hiện search theo tiêu đề bài báo bất kì", async () => {
         await page.locator("//input[@id='search']").fill(newsPapers[2].title);
+    });
+});
+let content;
+let title;
+test("Exercise 4 - Type 2", async ({ page }) => {
+
+    await test.step("1. Access to https://vnexpress.net/khoa-hoc", async () => {
+        await page.goto("https://vnexpress.net/khoa-hoc", { waitUntil: "domcontentloaded" });
+    });
+
+    await test.step("2. Get Titles and Contents", async () => {
+        title = await page.locator("//article/h3[@class='title-news']/a").allTextContents();
+        content = await page.locator("//article/p[@class='description']").allTextContents();
+    });
+
+    await test.step("3. Access https://material.playwrightvn.com/ and go to Personal Notes page", async () => {
+        await page.goto("https://material.playwrightvn.com/");
+        await page.locator("//a[@href='04-xpath-personal-notes.html']").click();
+    });
+
+    await test.step("4. Add 10 notes", async () => {
+        for (let i = 0; i < 10; i++) {
+            await page.locator("//input[@id='note-title']").fill(title[i]);
+            await page.locator("//textarea[@id='note-content']").fill(content[i]);
+            await page.locator("//button[@id='add-note']").click();
+        }
+    });
+
+    await test.step("5. Search random page title page", async () => {
+        await page.locator("//input[@id='search']").fill(title[1].trim());
     });
 });
